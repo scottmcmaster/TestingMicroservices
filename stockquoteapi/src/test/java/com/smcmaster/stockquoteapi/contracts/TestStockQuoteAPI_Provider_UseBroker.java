30918633030644d4d416fa59c894d00ce8976262
Scott McMaster <scott.d.mcmaster@gmail.com>
2018-09-29 13:20:12 -0700

    Almost get pact-broker working

--- stockquoteapi/src/test/java/com/smcmaster/stockquoteapi/contracts/TestStockQuoteAPI_Provider_UseBroker.java
+++ stockquoteapi/src/test/java/com/smcmaster/stockquoteapi/contracts/TestStockQuoteAPI_Provider_UseBroker.java
@@ -10,15 +10,15 @@ import com.smcmaster.stockquoteapi.Application;
 import au.com.dius.pact.provider.junit.PactRunner;
 import au.com.dius.pact.provider.junit.Provider;
 import au.com.dius.pact.provider.junit.State;
-import au.com.dius.pact.provider.junit.loader.PactFolder;
+import au.com.dius.pact.provider.junit.loader.PactBroker;
 import au.com.dius.pact.provider.junit.target.HttpTarget;
 import au.com.dius.pact.provider.junit.target.Target;
 import au.com.dius.pact.provider.junit.target.TestTarget;
 
 @RunWith(PactRunner.class)
 @Provider("stockquoteapi")
-@PactFolder("pacts")
-public class TestStockQuoteAPI_Provider {
+@PactBroker(host = "localhost", port = "32769")
+public class TestStockQuoteAPI_Provider_UseBroker {
 
 	@TestTarget
 	public final Target target = new HttpTarget("http", "localhost", 8090);
